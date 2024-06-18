44d4c0627f45c1baa3c9550a8ba6789b90ead95c
Saksham Bhutani <saksham.bhutani1@gmail.com>
2024-06-19 00:17:28 +0530

    Create video_data.py

---
+++ video_data.py
@@ -0,0 +1,7 @@
+# video_data.py
+
+video_data = [
+    {
+        "url": "https://file.notion.so/f/f/36f12688-0f50-4772-9fd9-b034350e9fb3/208137b0-5e86-4d12-b66f-dcf94cc43c62/solution-video-new.mp4?id=83be1286-fcab-4e46-b2c0-51001c3e4f65&table=block&spaceId=36f12688-0f50-4772-9fd9-b034350e9fb3&expirationTimestamp=1718784000000&signature=3nMua-OxHZx7FLtxLs6wk2ew03d81osuHSUkyB5V2Q4&downloadName=solution-video-new.mp4",
+        "tags": ["All stakeholders including retailers, vendors, dealers, brands and end users empower each other with Blubirch"]
+    }
